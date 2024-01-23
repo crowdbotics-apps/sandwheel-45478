b2048e6eb726e79f08659fed4c9acea8183cad3b
Crowdbotics <team@crowdbotics.com>
2024-01-23 01:33:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sandwheel-45478",
+  url: "https://sandwheel-45478.botics.co",
+  api: "https://sandwheel-45478.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
